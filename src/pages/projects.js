4234f9ab0c7ef52fb7ca3c5c4e331097cb713d30
Anderson Navarro <andersonaugustonb@ufps.edu.co>
2023-08-06 14:33:32 -0500

    FEATURE: Se termina la sección de articulos, empezaremos con el darkTheme

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from "framer-motion"
 import Head from "next/head"
 import Layout from "@/components/Layout"
 import AnimatedText from "@/components/AnimatedText"
@@ -8,6 +9,7 @@ import { GithubIcon } from "@/components/Icons"
 import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg"
 
 
+const FramerImage = motion(Image);
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
     return (
         <article className="w-full flex items-center justify-between relative
@@ -18,7 +20,10 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
             <Link href={ link } target="_blank"
                 className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
             >
-                <Image src={ img } alt={ title } className="w-full h-auto" />
+                <FramerImage src={ img } alt={ title } className="w-full h-auto"
+                    whileHover={ { scale: 1.05 } }
+                    transition={ { duration: 0.2 } }
+                />
             </Link>
             <div className="w-1/2 flex flex-col items-start justify-between pl-6">
                 <span className="text-primary font-medium text-xl">
@@ -51,7 +56,10 @@ const Project = ({ title, type, img, link, github }) => {
             <Link href={ link } target="_blank"
                 className="w-full cursor-pointer overflow-hidden rounded-lg"
             >
-                <Image src={ img } alt={ title } className="w-full h-auto" />
+                <FramerImage src={ img } alt={ title } className="w-full h-auto"
+                    whileHover={ { scale: 1.05 } }
+                    transition={ { duration: 0.2 } }
+                />
             </Link>
             <div className="w-full flex flex-col items-start justify-between mt-4">
                 <span className="text-primary font-medium text-xl">
